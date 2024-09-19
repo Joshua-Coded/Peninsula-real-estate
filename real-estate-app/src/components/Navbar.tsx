348940aa9977daabfaa0329ef3053d05b3dfd425
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-19 12:25:12 +0200

    hamburger

--- real-estate-app/src/components/Navbar.tsx
+++ real-estate-app/src/components/Navbar.tsx
@@ -33,49 +33,50 @@ const Navbar: React.FC = () => {
         <nav
             className={`fixed w-full top-0 z-50 transition-all ${isMobile || scrollPosition > 100 ? "bg-primary" : "bg-transparent"
                 }`}
-            style={{ paddingTop: "10px", paddingBottom: "10px" }} // Added padding for top and bottom
+            style={{ paddingTop: "10px", paddingBottom: "10px" }}
         >
             <div className="w-full px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
-                    {/* Logo on the left */}
-                    <div className="flex-shrink-0 text-white text-2xl font-bold">
+                    {/* Logo */}
+                    <div className="flex-shrink-0">
                         <Link to="/">
                             <img
                                 src={logo}
                                 alt="Logo"
                                 className="h-15 w-auto md:h-16 md:w-auto"
-                                style={{ width: "100%", height: "100%", marginLeft: isMobile ? "-10%" : "0" }} // Ensure the logo fits nicely on mobile
+                                style={{ width: "100%", height: "100%", marginLeft: isMobile ? "-10%" : "0" }}
                             />
                         </Link>
                     </div>
 
-                    {/* Desktop Menu in the center */}
+                    {/* Desktop Menu */}
                     <div className="hidden md:flex flex-grow justify-center space-x-6 text-sm">
-                        <Link to="/" className="text-white hover:border-b-2 border-white transition-all">
+                        <Link to="/" className="text-white hover:border-b-2 border-white transition-all" style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>
                             Home
                         </Link>
-                        <Link to="/about" className="text-white hover:border-b-2 border-white transition-all">
+                        <Link to="/about" className="text-white hover:border-b-2 border-white transition-all" style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>
                             About
                         </Link>
-                        <Link to="/projects" className="text-white hover:border-b-2 border-white transition-all">
+                        <Link to="/projects" className="text-white hover:border-b-2 border-white transition-all" style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>
                             Our Projects
                         </Link>
-                        <Link to="/services" className="text-white hover:border-b-2 border-white transition-all">
+                        <Link to="/services" className="text-white hover:border-b-2 border-white transition-all" style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>
                             Services
                         </Link>
-                        <Link to="/blog" className="text-white hover:border-b-2 border-white transition-all">
+                        <Link to="/blog" className="text-white hover:border-b-2 border-white transition-all" style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>
                             Blog
                         </Link>
-                        <Link to="/contact" className="text-white hover:border-b-2 border-white transition-all">
-                            Contact
+                        <Link to="/contact" className="text-white hover:border-b-2 border-white transition-all" style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>
+                            Contact Us
                         </Link>
                     </div>
 
-                    {/* Explore Button on the right */}
+                    {/* Explore Button */}
                     <div className="hidden md:block">
                         <Link
                             to="/explore"
                             className="bg-primary text-white py-2 px-4 rounded-md hover:bg-white hover:text-primary transition-all"
+                            style={{ color: 'white', fontFamily: 'Open Sans', fontSize: '18px', fontWeight: '600' }}
                         >
                             Explore
                         </Link>
@@ -112,19 +113,19 @@ const Navbar: React.FC = () => {
                 >
                     {/* Top Row with Logo and Close Button */}
                     <div className="flex justify-between items-center w-full px-4 py-4">
-                        {/* Logo on the left */}
+                        {/* Logo */}
                         <div className="flex-shrink-0 text-white text-2xl font-bold">
                             <Link to="/">
                                 <img
                                     src={logo}
                                     alt="Logo"
                                     className="h-10 w-auto"
-                                    style={{ width: "100%", height: "100%", marginLeft: "10%" }} // 10% space on mobile left
+                                    style={{ width: "100%", height: "100%", marginLeft: "10%" }}
                                 />
                             </Link>
                         </div>
 
-                        {/* Close Button on the right */}
+                        {/* Close Button */}
                         <button
                             onClick={() => setIsOpen(false)}
                             className="text-white focus:outline-none"
@@ -146,25 +147,25 @@ const Navbar: React.FC = () => {
                         </button>
                     </div>
 
-                    {/* Navigation Links with Desktop Font Sizes */}
+                    {/* Navigation Links */}
                     <ul className="flex flex-col space-y-8 text-sm w-full px-8 mt-10 text-normal font-normal">
                         <li className="border-b border-white hover:border-b-2 transition-all">
-                            <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+                            <Link to="/" onClick={() => setIsOpen(false)} style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>Home</Link>
                         </li>
                         <li className="border-b border-white hover:border-b-2 transition-all">
-                            <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+                            <Link to="/about" onClick={() => setIsOpen(false)} style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>About</Link>
                         </li>
                         <li className="border-b border-white hover:border-b-2 transition-all">
-                            <Link to="/projects" onClick={() => setIsOpen(false)}>Our Projects</Link>
+                            <Link to="/projects" onClick={() => setIsOpen(false)} style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>Our Projects</Link>
                         </li>
                         <li className="border-b border-white hover:border-b-2 transition-all">
-                            <Link to="/services" onClick={() => setIsOpen(false)}>Services</Link>
+                            <Link to="/services" onClick={() => setIsOpen(false)} style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>Services</Link>
                         </li>
                         <li className="border-b border-white hover:border-b-2 transition-all">
-                            <Link to="/blog" onClick={() => setIsOpen(false)}>Blog</Link>
+                            <Link to="/blog" onClick={() => setIsOpen(false)} style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>Blog</Link>
                         </li>
                         <li className="border-b border-white hover:border-b-2 transition-all">
-                            <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+                            <Link to="/contact" onClick={() => setIsOpen(false)} style={{ color: 'rgba(255, 255, 255, 0.70)', fontFamily: 'Open Sans', fontSize: '16px', fontWeight: '400' }}>Contact Us</Link>
                         </li>
                     </ul>
 
@@ -174,6 +175,7 @@ const Navbar: React.FC = () => {
                             to="/explore"
                             className="border-4 border-white text-white py-2 px-6 rounded-md w-3/4 text-center"
                             onClick={() => setIsOpen(false)}
+                            style={{ color: 'white', fontFamily: 'Open Sans', fontSize: '18px', fontWeight: '600' }}
                         >
                             Explore
                         </Link>
